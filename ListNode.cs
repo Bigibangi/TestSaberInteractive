d071aca1150d4788ee7cd242b5e06683ebe953f9
vtito <vtito@DESKTOP-LIITKTH>
2022-01-17 21:35:02 +0300

    Добавьте файлы проекта.

---
+++ ListNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializeTestSaber
+{
+    public class ListNode
+    {
+        public ListNode Prev;
+        public ListNode Next;
+        public ListNode Rand;
+        public string Data;
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ListNode);
+        }
+
+        public bool Equals(ListNode other)
+        {
+            return other != null &&
+                   other.Prev == this.Prev &&
+                   other.Next == this.Next &&
+                   other.Rand == this.Rand &&
+                   other.Data.Equals(this.Data);
+        }
+    }
+}
