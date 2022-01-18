4792f4481f425c7037df491c8e33340eb5426167
Влад Титовский <vtitovskiy@gmail.com>
2022-01-18 07:21:30 +0300

    Override ListNode.GetHashhCode

--- ListNode.cs
+++ ListNode.cs
@@ -24,5 +24,10 @@ namespace SerializeTestSaber
                    other.Rand == this.Rand &&
                    other.Data.Equals(this.Data);
         }
+
+        public override int GetHashCode()
+        {
+            return Data.Length * 13;
+        }
     }
 }
